6f417c7034b414a841122ca7277bbfdb1e2accfa
Glitch (joegaffey-web) <none>
2023-01-03 17:56:44 +0000

    Added RSS feed etc

--- main.js
+++ main.js
@@ -2,36 +2,30 @@ let darkTheme = false;
 if(document.body.classList.contains('dark'))
   darkTheme = true;
 
-const MASTODON_RSS_URL = `https://mastodon.social/users/joegaffey.rss`;
+const RSS_URL = `./feed.rss`;
 
-function getDescription(html) {
-  let doc = new DOMParser().parseFromString(html, "text/html");
-  doc = new DOMParser().parseFromString(doc.documentElement.textContent, "text/html");
-  const pEl = doc.querySelector("p");
-  const linkEl = pEl.querySelector("a[rel~=noopener]");
-  if(linkEl)
-    linkEl.innerText = 'More...'        
-  return `<p>${pEl.innerHTML}</p>`;
-}
-
-function getRSS(RSS_URL) {
-  fetch(RSS_URL) 
-    .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then(data => {
-      const items = data.querySelectorAll("item");
-      let html = `<div class="posts">`;
-      items.forEach(el => {
-        const rssDesc = getDescription(el.querySelector("description").innerHTML);
-        const rssLink = el.querySelector("link").innerHTML;
-        const rssDate = new Date(el.querySelector("pubDate").innerHTML).toLocaleString();
-
-        html += `<h3><a href=${rssLink}>${rssDate}</a></h3>${rssDesc}`;
-      });
-      html += `</div>`;
-      document.querySelector('.currentCard').insertAdjacentHTML("beforeend", html);
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach(el => {
+      html += `
+          <h3>
+            ${el.querySelector("title").innerHTML}
+          </h3>
+          <p>
+            ${el.querySelector("description").innerHTML}
+            <a href="${el.querySelector("link").innerHTML}">More..</a>
+          </p>          
+      `;
     });
-}
+    document.querySelector('.posts').insertAdjacentHTML("beforeend", html);
+  });
+
+CSS.paintWorklet.addModule('./particles.js');
+
 
 // getRSS(MASTODON_RSS_URL);
 
@@ -49,4 +43,33 @@ function getRSS(RSS_URL) {
 //     updateSketchfabCard();
 // }
 
-CSS.paintWorklet.addModule('./particles.js')
+// const MASTODON_RSS_URL = 'https://joegaffey-web.glitch.me/feed.rss';
+
+// function getDescription(html) {
+//   let doc = new DOMParser().parseFromString(html, "text/html");
+//   doc = new DOMParser().parseFromString(doc.documentElement.textContent, "text/html");
+//   const pEl = doc.querySelector("p");
+//   const linkEl = pEl.querySelector("a[rel~=noopener]");
+//   if(linkEl)
+//     linkEl.innerText = 'More...'        
+//   return `<p>${pEl.innerHTML}</p>`;
+// }
+
+// function getRSS(RSS_URL) {
+//   fetch(RSS_URL) 
+//     .then(response => response.text())
+//     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+//     .then(data => {
+//       const items = data.querySelectorAll("item");
+//       let html = `<div class="posts">`;
+//       items.forEach(el => {
+//         const rssDesc = getDescription(el.querySelector("description").innerHTML);
+//         const rssLink = el.querySelector("link").innerHTML;
+//         const rssDate = new Date(el.querySelector("pubDate").innerHTML).toLocaleString();
+
+//         html += `<h3><a href=${rssLink}>${rssDate}</a></h3>${rssDesc}`;
+//       });
+//       html += `</div>`;
+//       document.querySelector('.currentCard').insertAdjacentHTML("beforeend", html);
+//     });
+// }
